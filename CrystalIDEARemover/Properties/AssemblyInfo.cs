322154ce592e0af96f28f18aeb1a82e7e042bcff
Powerstro <pattiann.fg06061994@outlook.com>
2026-07-21 21:35:18 +0300

    Add files via upload

---
+++ CrystalIDEARemover/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General assembly information is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with the assembly.
+[assembly: AssemblyTitle("CrystalIDEARemover")]
+[assembly: AssemblyDescription("CrystalIDEA advanced removal and cleanup utility for Windows")]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: AssemblyCompany("CrystalIDEA Tools")]
+[assembly: AssemblyProduct("CrystalIDEARemover")]
+[assembly: AssemblyCopyright("Copyright 2021-2026 CrystalIDEA Tools")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly invisible
+// to COM components. If you need to access a type in this assembly from COM,
+// set the ComVisible attribute on that type to true.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("7c2a1f48-3b9e-4d17-9aa6-2e5f1b08c7d3")]
+
+// Assembly version information consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all values or use the default for Build and Revision
+// by using the '*' character:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("3.8.1.0")]
+[assembly: AssemblyFileVersion("3.8.1.0")]
